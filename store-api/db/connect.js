d2e61a2fc7863ac86d3980dd74b809759ff1096c
Yosef Alemu <yosefalemumengstie@gmail.com>
2023-07-17 13:43:07 +0300

    final commit

---
+++ store-api/db/connect.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const connectDB = (url) => {
+  return mongoose
+    .connect(url, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log(`connected to DB...`))
+    .catch((error) => console.log(error));
+};
+
+module.exports = connectDB;
